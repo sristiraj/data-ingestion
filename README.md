4ce2d76ecd298036350743ef80fd7c63d74ed8e7
rajsr1 <sristi.raj@novartis.com>
2021-11-14 16:20:41 +0530

    Added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# data-ingestion
+Add glue scripts are stored under glue-scripts folder
+
+Add below params to the glue jobs
+
+--JOB_NAME - Glue job name
+
+--INPUT_DATA_PATH - Input data path in s3
+--SOURCE_FORMAT - Input file format like csv, parquet etc
+--INPUT_SCHEMA_PATH - Input data schema file in avro schema format (file should end with .avsc)
+
+--OUTPUT_DATA_PATH - Output data path in s3
+--OUTPUT_SCHEMA_PATH - Output data schema file path in s3
+--TARGET_FORMAT - Target format of data
+--GLUE_DB_NAME - Glue database name within table creation
+--GLUE_TABLE_NAME - Glue table name for the table creation
---
+++ glue-scripts/raw_ingestion.py
@@ -0,0 +1,78 @@
+import sys
+from pyspark.sql import SparkSession
+from pysparkutil.common.schema import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from pyspark.sql.functions import *
+from watcherlogger.logger import watcherlogger
+from datetime import datetime
+
+
+args = getResolvedOptions(sys.argv, ["JOB_NAME","INPUT_DATA_PATH","OUTPUT_DATA_PATH","GLUE_DB_NAME","GLUE_TABLE_NAME","INPUT_SCHEMA_PATH","OUTPUT_SCHEMA_PATH","SOURCE_FORMAT","TARGET_FORMAT"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+
+class RawDataSource(object):
+    def __init__(self, params):
+        self.params = params
+        print(self.params)
+    def read_raw_data(self):
+        schema_obj = Schema()
+        print(self.params["input_schema_path"])
+        schema_obj.set_location(self.params["input_schema_path"])
+        
+        schema = schema_obj.get_schema()
+        print(schema)
+        df = spark.read.format(self.params["source_format"]).option("header","true").schema(schema).load(self.params["input_data_path"])
+        return df
+
+class DataSink(object):
+    def __init__(self, params):
+        self.params = params
+    def write_target_data(self, df):
+        schema_obj = Schema()
+        schema_obj.set_location(self.params["output_schema_path"])
+        schema = schema_obj.get_schema()
+        df = spark.createDataFrame(df.rdd,schema=schema)
+        df.write.format(self.params["target_format"]).mode("overwrite").option("path",self.params["output_data_path"]).saveAsTable(self.params["catalog_db"]+"."+self.params["catalog_table"])
+        
+class AuditFields(object):
+    def __init__(self):
+        self.audit_fields = ["audit_created_tmstmp","audit_updated_tmstmp","audit_created_by","audit_updated_by","audit_data_source"]
+    def add_audit_created_tmstmp(self,df,col_name):
+        return df.withColumn(col_name, lit(datetime.now()))
+    def add_audit_updated_tmstmp(self,df,col_name):
+        return df.withColumn(col_name, lit(datetime.now()))  
+    def add_audit_created_by(self,df,col_name):
+        return df.withColumn(col_name, lit("glue"))
+    def add_audit_updated_by(self,df,col_name):
+        return df.withColumn(col_name, lit("glue"))  
+    def add_audit_data_source(self, df, col_name):
+        return df.withColumn(col_name, lit("slr"))  
+    def add_audit(self, df):
+        for col in self.audit_fields:
+            col_name = "add_"+col
+            fn = getattr(self, col_name)
+            df = fn(df,col)
+        return df    
+        
+if __name__ == "__main__":
+    
+    context = {"job_name":args["JOB_NAME"], "service_arn":"SLRLoadMetric", "module_name":"SLR", "job_type":"full"}
+    logger = watcherlogger().Builder().setLogLevel(logging.INFO).setStreamNamePrefix(context["module_name"]).getOrCreate()
+    print("Started Job")
+    #Separate read and write params
+    read_params = {"input_data_path":args["INPUT_DATA_PATH"],"input_schema_path":args["INPUT_SCHEMA_PATH"],"source_format":args["SOURCE_FORMAT"]}
+    write_params = {"output_data_path":args["OUTPUT_DATA_PATH"], "output_schema_path":args["OUTPUT_SCHEMA_PATH"],"catalog_db":args["GLUE_DB_NAME"],"catalog_table":args["GLUE_TABLE_NAME"],"target_format":args["TARGET_FORMAT"]}
+    log_data = context
+    print("Read source")
+    #Read source data
+    source = RawDataSource(read_params).read_raw_data()
+    #Added audit fields
+    source_with_audit = AuditFields().add_audit(source)
+    
+    #Write target data
+    DataSink(write_params).write_target_data(source_with_audit)
+    
