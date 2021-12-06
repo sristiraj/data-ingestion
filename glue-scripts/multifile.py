b7dc0b80a7fd61d644cd98e77d77618ae9a8804b
sristiraj <sristiraj.a@gmail.com>
2021-12-06 12:15:33 +0530

    Update multifile.py

--- glue-scripts/multifile.py
+++ glue-scripts/multifile.py
@@ -34,16 +34,18 @@ in_bucket = input_data
 def fun(f):
   file_orig=f[0]
   filepath = f[0]
+  print(filepath)
   data = f[1]
-  df = pd.read_csv(StringIO(data),header=[0],delimiter=",")
-  df = df.replace(r'\n','', regex=True) 
-  df = df.to_csv(header=True, index=False, quoting = csv.QUOTE_ALL).strip('\n').split('\n')
-  df_string = '\r\n'.join(df)
-  df_bytes = df_string.encode('utf8')
   s3 = boto3.resource('s3')
   outpath = filepath.replace(input_data+"/"+input_prefix,output_data+"/"+output_prefix)
   object = s3.Object(output_data, outpath.replace("s3://","").replace(output_data+"/",""))
-  object.put(Body=df_bytes)
+  dfreader = pd.read_csv(StringIO(data),header=[0],delimiter=",",chunksize=1000)
+  for chunk in dfreader:
+      df = chunk.replace(r'\n','', regex=True) 
+      df = df.to_csv(header=True, index=False, quoting = csv.QUOTE_ALL).strip('\n').split('\n')
+      df_string = '\r\n'.join(df)
+      df_bytes = df_string.encode('utf8')
+      object.put(Body=df_bytes)
   return (filepath)
 
 
@@ -59,6 +61,7 @@ for index, file in enumerate(contents):
 print(list_of_files)
 rdd_all = sc.wholeTextFiles(",".join(list_of_files))
 # rdd_out = rdd_all.foreach(fun)
-rows = rdd_all.collect()
-rdd_out = rdd_all.map(fun)  
-print(rdd_out.collect())
+rdd_all_repart = rdd_all.repartition(len(list_of_files))
+print(rdd_all_repart.getNumPartitions())
+rdd_out = rdd_all_repart.map(fun)  
+print(rdd_all_repart.count())
