cd0c05a16dace9dd9cd2d0605cee9584bf69a453
sristiraj <sristiraj.a@gmail.com>
2022-02-02 18:36:43 +0530

    Update participant_5555.sql

--- sql/participant_5555.sql
+++ sql/participant_5555.sql
@@ -1,8 +1,8 @@
-select distinct unpiv.plan_num, unpiv.part_num, unpiv.part_subplan, unpiv.part_extension,unpiv.plan_year, unpiv.activity, 
+select distinct unpiv.part_ssn, unpiv.plan_year, unpiv.activity, 
 case when temp_mapping.Employee is not null AND  temp_mapping.Employee != '' then unpiv.value end Employee,
 case when temp_mapping.Automatic is not null AND  temp_mapping.Automatic != '' then unpiv.value end Automatic, 
 case when temp_mapping.Matching is not null AND  temp_mapping.Matching != '' then unpiv.value end Matching from (
-select plan_num, part_num, part_subplan, part_extension,plan_year, stack(11,'401 EE contribution',cntrb_401k_ee,'415 Contribution',cntrb_415,'401K Catchup',cntrb_401k_ctchup,'Roth Contribution',
+select part_num as part_ssn, plan_year, stack(11,'401 EE contribution',cntrb_401k_ee,'415 Contribution',cntrb_415,'401K Catchup',cntrb_401k_ctchup,'Roth Contribution',
 cntrb_roth, 'agency Match', agency_match, 'Agency Automatic 1%, 2 year vested', agency_auto_2yr, 'Employee Tax-Exempt', part_tax_exempt,  'Employee Tax-Defered', part_tax_defer, 'Part Tax Defferred Rollover', roll_in_tax_excempt , 'Roth Deferred', part_roth, 'Participant Roth Rollover', roll_in_roth) as (activity,value) from piv) unpiv join temp_mapping
 on unpiv.activity = temp_mapping.Activity 
-order by plan_num, part_num, part_subplan, part_extension, plan_year,activity
+order by 1,2,3
