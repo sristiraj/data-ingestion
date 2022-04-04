7045ede0c9ed30cf342d7e3a23e3ddc20b2301db
sristiraj <sristiraj.a@gmail.com>
2022-04-04 21:21:55 +0530

    Update create_participant_balance_summary.sql

--- sql/create_participant_balance_summary.sql
+++ sql/create_participant_balance_summary.sql
@@ -13,7 +13,7 @@ create table datamart.participant_balance_summary(
   fundId varchar(100),
   subfundid varchar(100),
   accountId varchar(100),
-  fund_balance_units double,
+  fund_balance_units decimal(22,7),
   price varchar(100),
   civilian_group varchar(100),
   uniformed_group varchar(100)
