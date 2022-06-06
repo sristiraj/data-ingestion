35b100820788c519a30373e6703f1bf6a0e91a60
sristiraj <sristiraj.a@gmail.com>
2022-06-06 22:59:09 +0530

    Update participant_history.sql

--- sql/participant_history.sql
+++ sql/participant_history.sql
@@ -7,15 +7,15 @@ hash_key,
 create_date,
 update_date,
 update_userid,
-max(cast(participant_firstname varchar(100))) as participant_firstname,
-max(cast(participant_lastname varchar(100))) as participant_lastname,
-max(case(participant_middlename varchar(100))) as participant_middlename,
+max(cast(participant_firstname as varchar(100))) as participant_firstname,
+max(cast(participant_lastname as varchar(100))) as participant_lastname,
+max(cast(participant_middlename as varchar(100))) as participant_middlename,
 max(cast(maritalStatus as varchar(100))) as maritalStatus,
 max(cast(gender as varchar(100))) as gender,
 max(cast(ssn as varchar(100))) as ssn,
 max(cast(birth_date as varchar(100))) as birth_date,
 age,
-max(cast(department_code as varchar(10))) department_code.
+max(cast(department_code as varchar(10))) department_code,
 max(cast(employer_agency_code as varchar(10))) employer_agency_code,
 max(cast(payroll_office_number as varchar(100))) payroll_office_number,
 max(cast(civilian_scd_date as varchar(100))) civilian_scd_date,
@@ -27,8 +27,8 @@ max(cast(employment_status_effective_date as varchar(100))) employment_status_ef
 max(cast(retirement_code_civil as varchar(100))) retirement_code_civil,
 max(cast(retirement_catcode_civil as varchar(100))) retirement_catcode_civil,
 max(cast(retirement_code_uniformed as varchar(100))) retirement_code_uniformed,
-max(cast(retirement_catcode_uniformed as varchar(100)) retirement_catcode_uniformed,
-max(cast(retirementDate as varchar(100)) retirementDate,
+max(cast(retirement_catcode_uniformed as varchar(100))) retirement_catcode_uniformed,
+max(cast(retirementDate as varchar(100))) retirementDate,
 max(cast(hireDate as varchar(100))) hireDate,
 max(cast(isTerminated as varchar(10))) isTerminated,
 max(cast(terminationDate as varchar(100))) terminationDate,
@@ -53,12 +53,9 @@ udp.message_header_messagetimestamp,
 row_number() over(partition by idm.message_body_create_idmapping_val_platforminternalid order by 
 to_timestamp(case when udp.message_header_messagetimestamp="" then null else udp.message_header_messagetimestamp end ,"yyyy-MM-dd'T'HH:mm:ss.SSSz") desc) rn
 from 
-(select * from prod_alight_trusted_tba.tba_udpdatachangeevents_root_message_body_create_idmapping
-where partition_load_dt_tmstmp=(select max(partition_load_dt_tmstmp) 
-from prod_alight_trusted_tba.tba_udpdatachangeevents_root_message_body_create_idmapping)  ) idm 
+(select * from prod_alight_trusted_tba.tba_udpdatachangeevents_root_message_body_create_idmapping) idm 
 left outer join 
-(select * from prod_alight_trusted_tba.tba_udpdatachangeevents_root where partition_load_dt_tmstmp=(select max(partition_load_dt_tmstmp) from
- prod_alight_trusted_tba.tba_udpdatachangeevents_root)  ) udp 
+(select * from prod_alight_trusted_tba.tba_udpdatachangeevents_root ) udp 
 on udp.message_body_create_idmapping = idm.id and udp.partition_load_dt_tmstmp = idm.partition_load_dt_tmstmp
 )
 where rn=1)
@@ -67,7 +64,7 @@ udp.message_body_create_globalpersonidentifier as global_participant_id,
 current_date as record_start_date,
 to_date('9999-09-09','yyyy-MM-dd') as record_end_date,
 cast(1 as long) as current_indicator,
-sha2(concat(udpdce.message_body_create_globalpersonidentifier,concat(rt.tbamessage_header_platforminternalid,rt.tbamessage_header_nationaltaxid))),256) as hash_kay,
+sha2(concat(udp.message_body_create_globalpersonidentifier,concat(rt.tbamessage_header_platforminternalid,rt.tbamessage_header_nationaltaxid)),256) as hash_key,
 current_date create_date,
 current_date as update_date,
 'glue' as update_userid,
@@ -83,8 +80,8 @@ case tbwabcoc.tbamessage_benefitsworkerattributes_val_benefitscurrentorganizatio
 When '8337' THEN tbwabcoc.tbamessage_benefitsworkerattributes_val_benefitscurrentorganizationcodes_val_value 
 When '8347' THEN tbwabcoc.tbamessage_benefitsworkerattributes_val_benefitscurrentorganizationcodes_val_value ELSE null END department_code,
 case tbwabcoc.tbamessage_benefitsworkerattributes_val_benefitscurrentorganizationcodes_val_organizationid
-WHEN '8357' THEN tbwabcoc.tbwabcoc.tbamessage_benefitsworkerattributes_val_benefitscurrentorganizationcodes_val_value
-WHEN '8367' THEN tbwabcoc.tbamessage_benefitsworkerattributes_val_benefitscurrentorganizationcodes_val_value ELSE null END employer_agency_code.
+WHEN '8357' THEN tbwabcoc.tbamessage_benefitsworkerattributes_val_benefitscurrentorganizationcodes_val_value
+WHEN '8367' THEN tbwabcoc.tbamessage_benefitsworkerattributes_val_benefitscurrentorganizationcodes_val_value ELSE null END employer_agency_code,
 case tbpavaa.tbamessage_personattributes_val_additionalattributes_val_fieldname
 WHEN 'C-PAYROLL-OFF-NUM' THEN tbpavaa.tbamessage_personattributes_val_additionalattributes_val_fieldvalue
 WHEN 'U-PAYROLL-OFF-NUM' THEN tbpavaa.tbamessage_personattributes_val_additionalattributes_val_fieldvalue 
@@ -122,30 +119,30 @@ case tbwabcs.tbamessage_benefitsworkerattributes_val_benefitcurrentemploymentsta
 WHEN '21' THEN tbwabcs.tbamessage_benefitsworkerattributes_val_benefitscurrentemploymentstatuses_val_begindate 
 WHEN '22' THEN tbwabcs.tbamessage_benefitsworkerattributes_val_benefitscurrentemploymentstatuses_val_begindate ELSE null END employment_cd_date,
 to_timestamp(case when rt.tbamessage_header_sourcesystemextractiontimestamp="" then null else rt.tbamessage_header_sourcesystemextractiontimestamp end ,"yyyy-MM-dd'T'HH:mm:ss.SSSz") sourcesystemextractiontimestamp,
-to_timestamp(case when udpdce.message_header_messagetimestamp="" then null else udpdce.message_header_messagetimestamp end ,"yyyy-MM-dd'T'HH:mm:ss.SSSz") messageTimestamp,
+to_timestamp(case when udp.message_header_messagetimestamp="" then null else udp.message_header_messagetimestamp end ,"yyyy-MM-dd'T'HH:mm:ss.SSSz") messageTimestamp,
 tbapbar.tbamessage_personbenefitaccountrelationships_val_originatingpersoninternalid originatingpersoninternalid
-FROM rt_header rt left outer join prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_benefitworkerattributes tbwa 
-on rt.tbamessage_benefitworkerattributes = tbwa.id and rt.partition_load_dt_tmstmp = tbwa.partition_load_dt_tmstmp left outer join 
-prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_benefitworkerattributes_val_benefitcurrentorganizationcodes tbwabcoc 
-on tbwa.tbamessage_benefitsworkerattributes_val_benefitscurrentorganizationcodes = tbwabococ.id and 
+FROM rt_header rt left outer join prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_benefitsworkerattributes tbwa 
+on rt.tbamessage_benefitsworkerattributes = tbwa.id and rt.partition_load_dt_tmstmp = tbwa.partition_load_dt_tmstmp left outer join 
+prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_benefitsworkerattributes_val_benefitscurrentorganizationcodes tbwabcoc 
+on tbwa.tbamessage_benefitsworkerattributes_val_benefitscurrentorganizationcodes = tbwabcoc.id and 
  tbwa.partition_load_dt_tmstmp = tbwabcoc.partition_load_dt_tmstmp
 left outer join prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_personattributes tbpa 
 on rt.tbamessage_personattributes = tbpa.id and rt.partition_load_dt_tmstmp = tbpa.partition_load_dt_tmstmp left outer join 
 prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_personattributes_val_additionalattributes tbpavaa 
 on tbpa.tbamessage_personattributes_val_additionalattributes = tbpavaa.id
 and tbpa.partition_load_dt_tmstmp = tbpavaa.partition_load_dt_tmstmp
-left outer join prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_benefitworkerattributes_val_benefitcurrentemploymentstatuses  tbwabcs 
-on tbwa.tbamessage_benefitsworkerattributes_val_benefitcurrentemploymentstatuses = tbwabcs.id 
+left outer join prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_benefitsworkerattributes_val_benefitscurrentemploymentstatuses  tbwabcs 
+on tbwa.tbamessage_benefitsworkerattributes_val_benefitscurrentemploymentstatuses = tbwabcs.id 
 and tbwa.partition_load_dt_tmstmp = tbwabcs.partition_load_dt_tmstmp
 left outer join 
-udpdce idm on rt.tbamessage_header_platforminternalid = 
-idm.message_body_create_idmapping_val_platforminternalid
-left outer join  prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_personbenefitaccountrelationships tbapbar
+udpdce udp on rt.tbamessage_header_platforminternalid = 
+udp.message_body_create_idmapping_val_platforminternalid
+left outer join  prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_personbenefitsaccountrelationships tbapbar
 on rt.tbamessage_personbenefitaccountrelationships = tbapbar.id
 and rt.partition_load_dt_tmstmp = tbapbar.partition_load_dt_tmstmp
 left outer join 
 prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_preferences tbmsgpref 
-on rt.tbamessage_perferences = tbmsgpref.id 
+on rt.tbamessage_preferences = tbmsgpref.id 
 and rt.partition_load_dt_tmstmp = tbmsgpref.partition_load_dt_tmstmp
 left outer join
 prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_preferences_val_communicationpreferences tbmsgcommpref on 
@@ -166,12 +163,9 @@ udp.message_header_messagetimestamp,
 row_number() over(partition by idm.message_body_update_after_modificationhistory_val_sourcesysteminternalid order by 
 to_timestamp(case when udp.message_header_messagetimestamp="" then null else udp.message_header_messagetimestamp end ,"yyyy-MM-dd'T'HH:mm:ss.SSSz") desc) rn
 from 
-(select * from prod_alight_trusted_tba.tba_udpdatachangeevents_root_message_body_update_after_modificationhistory
-where partition_load_dt_tmstmp=(select max(partition_load_dt_tmstmp) 
-from prod_alight_trusted_tba.tba_udpdatachangeevents_root_message_body_update_after_modificationhistory)  ) idm 
+(select * from prod_alight_trusted_tba.tba_udpdatachangeevents_root_message_body_update_after_modificationhistory) idm 
 left outer join 
-(select * from prod_alight_trusted_tba.tba_udpdatachangeevents_root where partition_load_dt_tmstmp=(select max(partition_load_dt_tmstmp) from
- prod_alight_trusted_tba.tba_udpdatachangeevents_root)  ) udp 
+(select * from prod_alight_trusted_tba.tba_udpdatachangeevents_root ) udp 
 on udp.message_body_after_modificationhistory  = idm.id and udp.partition_load_dt_tmstmp = idm.partition_load_dt_tmstmp)
 where rn=1)
 select distinct rt.tbamessage_header_platforminternalid as participant_id,
@@ -179,7 +173,7 @@ udp.message_body_update_after_globalpersonidentifier as global_participant_id,
 current_date as record_start_date,
 to_date('9999-09-09','yyyy-MM-dd') as record_end_date,
 cast(1 as long) as current_indicator,
-sha2(concat(udp.message_body_update_after_globalpersonidentifier,concat(rt.tbamessage_header_platforminternalid,rt.tbamessage_header_nationaltaxid))),256) as hash_kay,
+sha2(concat(udp.message_body_update_after_globalpersonidentifier,concat(rt.tbamessage_header_platforminternalid,rt.tbamessage_header_nationaltaxid)),256) as hash_key,
 current_date create_date,
 current_date as update_date,
 'glue' as update_userid,
@@ -195,8 +189,8 @@ case tbwabcoc.tbamessage_benefitsworkerattributes_val_benefitscurrentorganizatio
 When '8337' THEN tbwabcoc.tbamessage_benefitsworkerattributes_val_benefitscurrentorganizationcodes_val_value 
 When '8347' THEN tbwabcoc.tbamessage_benefitsworkerattributes_val_benefitscurrentorganizationcodes_val_value ELSE null END department_code,
 case tbwabcoc.tbamessage_benefitsworkerattributes_val_benefitscurrentorganizationcodes_val_organizationid
-WHEN '8357' THEN tbwabcoc.tbwabcoc.tbamessage_benefitsworkerattributes_val_benefitscurrentorganizationcodes_val_value
-WHEN '8367' THEN tbwabcoc.tbamessage_benefitsworkerattributes_val_benefitscurrentorganizationcodes_val_value ELSE null END employer_agency_code.
+WHEN '8357' THEN tbwabcoc.tbamessage_benefitsworkerattributes_val_benefitscurrentorganizationcodes_val_value
+WHEN '8367' THEN tbwabcoc.tbamessage_benefitsworkerattributes_val_benefitscurrentorganizationcodes_val_value ELSE null END employer_agency_code,
 case tbpavaa.tbamessage_personattributes_val_additionalattributes_val_fieldname
 WHEN 'C-PAYROLL-OFF-NUM' THEN tbpavaa.tbamessage_personattributes_val_additionalattributes_val_fieldvalue
 WHEN 'U-PAYROLL-OFF-NUM' THEN tbpavaa.tbamessage_personattributes_val_additionalattributes_val_fieldvalue 
@@ -234,14 +228,14 @@ case tbwabcs.tbamessage_benefitsworkerattributes_val_benefitcurrentemploymentsta
 WHEN '21' THEN tbwabcs.tbamessage_benefitsworkerattributes_val_benefitscurrentemploymentstatuses_val_begindate 
 WHEN '22' THEN tbwabcs.tbamessage_benefitsworkerattributes_val_benefitscurrentemploymentstatuses_val_begindate ELSE null END employment_cd_date,
 to_timestamp(case when rt.tbamessage_header_sourcesystemextractiontimestamp="" then null else rt.tbamessage_header_sourcesystemextractiontimestamp end ,"yyyy-MM-dd'T'HH:mm:ss.SSSz") sourcesystemextractiontimestamp,
-to_timestamp(case when udpdce.message_header_messagetimestamp="" then null else udpdce.message_header_messagetimestamp end ,"yyyy-MM-dd'T'HH:mm:ss.SSSz") messageTimestamp,
+to_timestamp(case when udp.message_header_messagetimestamp="" then null else udp.message_header_messagetimestamp end ,"yyyy-MM-dd'T'HH:mm:ss.SSSz") messageTimestamp,
 tbapbar.tbamessage_personbenefitaccountrelationships_val_originatingpersoninternalid originatingpersoninternalid
 FROM rt_header1 rt left outer join 
-prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_benefitworkerattributes tbwa 
-on rt.tbamessage_benefitworkerattributes = tbwa.id 
+prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_benefitsworkerattributes tbwa 
+on rt.tbamessage_benefitsworkerattributes = tbwa.id 
 and rt.partition_load_dt_tmstmp = tbwa.partition_load_dt_tmstmp left outer join 
-prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_benefitworkerattributes_val_benefitcurrentorganizationcodes tbwabcoc 
-on tbwa.tbamessage_benefitsworkerattributes_val_benefitscurrentorganizationcodes = tbwabococ.id 
+prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_benefitsworkerattributes_val_benefitscurrentorganizationcodes tbwabcoc 
+on tbwa.tbamessage_benefitsworkerattributes_val_benefitscurrentorganizationcodes = tbwabcoc.id 
 and tbwa.partition_load_dt_tmstmp = tbwabococ.partition_load_dt_tmstmp
 left outer join prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_personattributes tbpa 
 on rt.tbamessage_personattributes = tbpa.id 
@@ -249,18 +243,18 @@ and rt.partition_load_dt_tmstmp = tbpa.partition_load_dt_tmstmp
 left outer join prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_personattributes_val_additionalattributes tbpavaa 
 on tbpa.tbamessage_personattributes_val_additionalattributes = tbpavaa.id
 and tbpa.partition_load_dt_tmstmp = tbpavaa.partition_load_dt_tmstmp
-left outer join prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_benefitworkerattributes_val_benefitcurrentemploymentstatuses  tbwabcs 
-on tbwa.tbamessage_benefitsworkerattributes_val_benefitcurrentemploymentstatuses = tbwabcs.id 
+left outer join prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_benefitsworkerattributes_val_benefitscurrentemploymentstatuses  tbwabcs 
+on tbwa.tbamessage_benefitsworkerattributes_val_benefitscurrentemploymentstatuses = tbwabcs.id 
 and  tbwa.partition_load_dt_tmstmp =  tbwabcs.partition_load_dt_tmstmp
 left outer join udpce1 udp 
 on rt.tbamessage_header_platforminternalid = 
 udp.message_body_update_after_modificationhistory_val_sourcesysteminternalid 
-left outer join  prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_personbenefitaccountrelationships tbapbar
-on rt.tbamessage_personbenefitaccountrelationships = tbapbar.id
+left outer join  prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_personbenefitsaccountrelationships tbapbar
+on rt.tbamessage_personbenefitsaccountrelationships = tbapbar.id
 and rt.partition_load_dt_tmstmp = tbapbar.partition_load_dt_tmstmp
 left outer join 
 prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_preferences tbmsgpref 
-on rt.tbamessage_perferences = tbmsgpref.id 
+on rt.tbamessage_preferences = tbmsgpref.id 
 and rt.partition_load_dt_tmstmp = tbmsgpref.partition_load_dt_tmstmp
 left outer join
 prod_alight_trusted_tba.tba_datachangeevents_root_tbamessage_preferences_val_communicationpreferences tbmsgcommpref on 
