f078b3a23bec2e66e7e30e1cf5a6f31b4fb6b698
sristiraj <sristiraj.a@gmail.com>
2022-08-13 16:05:16 +0530

    Update contact_delete_clause.py

--- glue-scripts/contact_delete_clause.py
+++ glue-scripts/contact_delete_clause.py
@@ -1,4 +1,4 @@
-pre_update_query="begin;delete from {} t1 where EXISTS (SELECT 1 FROM {} t2 WHERE".format(self.params["catalog_table"], self.params["stg_table"])
+pre_update_query="begin;delete from {}  where EXISTS (SELECT 1 FROM {} t2 WHERE".format(self.params["catalog_table"], self.params["stg_table"])
 for key in composite_key:
-    update_join_clause += "t1."+key+"="+"t2."+key+" and "
+    update_join_clause += self.params["catalog_table"]+"."+key+"="+"t2."+key+" and "
 update_join_clause +=  "1=1);"
