fad31a151f313a27483b2c3a4eb022fa361ca947
sristiraj <sristiraj.a@gmail.com>
2022-08-25 17:47:54 +0530

    Update checkpoint_ingestion_script.py

--- glue-scripts/checkpoint_ingestion_script.py
+++ glue-scripts/checkpoint_ingestion_script.py
@@ -1,7 +1,7 @@
 def transform(self):
         sql_query = self._schema_data()
         print(sql_query)
-        sql_query = sql_query.format(cdc_column=cdc_column)
+        sql_query = sql_query.format(cdc_column>=cdc_column)
         df = spark.sql(sql_query)
         print("Query successful")
         return df
