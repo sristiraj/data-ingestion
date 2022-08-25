f7a41943fff3abdab0d74e334db62a5968b4b8a7
sristiraj <sristiraj.a@gmail.com>
2022-08-25 17:53:06 +0530

    Update checkpoint_participant_hist.py

--- glue-scripts/checkpoint_participant_hist.py
+++ glue-scripts/checkpoint_participant_hist.py
@@ -1,17 +1,22 @@
-cdc_column = "partition_load_dt_tmstmp"
-checkpoint_val = datetime.now().strftime("%Y%m%d_%H%M%S")
-checkpoint_file = "s3://gluejobssubramani001/checkpoint/participant_history.checkpoint"
-#Checkpoint File storage bucket details
-checkpoint_bucket_index = checkpoint_file.replace("s3://","").replace("s3a://","").find("/")
-checkpoint_bucket = checkpoint_file[5:checkpoint_bucket_index+5]
-checkpoint_key = checkpoint_file[checkpoint_bucket_index+6:]
-
 class _checkpoint:
+    
+    checkpoint_val = datetime.now().strftime("%Y%m%d_%H%M%S")
+    checkpoint_file = "s3://gluejobssubramani001/checkpoint/participant_history.checkpoint"
+    #Checkpoint File storage bucket details
+    checkpoint_bucket_index = checkpoint_file.replace("s3://","").replace("s3a://","").find("/")
+    checkpoint_bucket = checkpoint_file[5:checkpoint_bucket_index+5]
+    checkpoint_key = checkpoint_file[checkpoint_bucket_index+6:]
     
     @classmethod
     def write_checkpoint(cls):
-        boto3.resource("s3").Object(checkpoint_bucket, checkpoint_key).put(Body='{"checkpoint_value":'+checkpoint_val+'}')
+        try:
+            boto3.resource("s3").Object(_checkpoint.checkpoint_bucket, _checkpoint.checkpoint_key).put(Body='{"checkpoint_value":'+_checkpoint.checkpoint_val+'}')
+        except:
+            return None
     @classmethod
     def read_checkpoint(cls):
-        return boto3.resource("s3").Object(checkpoint_bucket, checkpoint_key).get()['Body'].read().decode("utf-8") 
-    
+        try:
+            val = boto3.resource("s3").Object(_checkpoint.checkpoint_bucket, _checkpoint.checkpoint_key).get()['Body'].read().decode("utf-8") 
+        except:
+            val = None
+        return val    
