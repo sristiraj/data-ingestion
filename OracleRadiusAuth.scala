472aa947e216b042bab5ba1d8bd14e4bbe5616b0
sristiraj <sristiraj.a@gmail.com>
2022-10-06 20:48:50 +0530

    Create OracleRadiusAuth.scala

---
+++ OracleRadiusAuth.scala
@@ -0,0 +1,19 @@
+import java.util.Properties
+import org.apache.spark.sql.SparkSession
+
+
+def main(args: Array[String]): Unit ={
+	val spark = SparkSession
+      .builder()
+      .appName("Spark SQL data sources example")
+      .config("spark.some.config.option", "some-value")
+      .getOrCreate()
+	
+	val connectionProperties = new Properties()
+	connectionProperties.setProperty(OracleConnection.CONNECTION_PROPERTY_THIN_NET_AUTHENTICATION_SERVICES,
+      "("+AnoServices.AUTHENTICATION_RADIUS+")");
+    connectionProperties.setProperty("user", "username")
+    connectionProperties.setProperty("password", "password")
+	val url = "jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)"+ "(HOST=oracleserver.mydomain.com)(PORT=5561))(CONNECT_DATA=" + "(SERVICE_NAME=mydatabaseinstance)))
+	val jdbcDF2 = spark.read.jdbc(url, "schema.tablename", connectionProperties)
+}
